6206b71c80ee282248a3bdf9815ecc4291e23887
WelscherMarkus <markus.welscher01@gmail.com>
2024-12-02 05:58:54 +0100

    Add ssl/tls with certificate stored in aws secrets

--- go.mod
+++ go.mod
@@ -2,9 +2,25 @@ module Portfolio-Website
 
 go 1.23
 
-require github.com/gin-gonic/gin v1.10.0
+require (
+	github.com/aws/aws-sdk-go-v2 v1.32.6
+	github.com/aws/aws-sdk-go-v2/config v1.28.6
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.34.7
+	github.com/gin-gonic/gin v1.10.0
+)
 
 require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.47 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.25 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.24.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.2 // indirect
+	github.com/aws/smithy-go v1.22.1 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
--- server/main.go
+++ server/main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -27,8 +33,70 @@ func main() {
 			"message": "pong",
 		})
 	})
-	err = router.Run(":8080")
+
+	tlsConfig := loadTls()
+
+	server := &http.Server{
+		Addr:      ":8080",
+		Handler:   router,
+		TLSConfig: tlsConfig,
+	}
+
+	err = server.ListenAndServeTLS("", "")
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func loadTls() *tls.Config {
+	rawContent := getAwsSslCertificate()
+
+	log.Println(rawContent)
+
+	parts := strings.SplitAfter(rawContent, "-----END PRIVATE KEY-----")
+	if len(parts) != 2 {
+		log.Fatalf("failed to split the private key and certificate")
+	}
+	privateKey := parts[0]
+	certificate := parts[1]
+
+	cert, err := tls.X509KeyPair([]byte(certificate), []byte(privateKey))
+	if err != nil {
+		log.Fatalf("failed to parse key pair: %v", err)
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM([]byte(certificate)) {
+		log.Fatalf("failed to append certificate to pool")
+	}
+
+	return &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      certPool,
+	}
+}
+
+func getAwsSslCertificate() string {
+	secretName := "Webserver-SSL"
+	region := "eu-north-1"
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svc := secretsmanager.NewFromConfig(cfg)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"),
+	}
+
+	result, err := svc.GetSecretValue(context.TODO(), input)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var secretString string = *result.SecretString
+	return secretString
 }
